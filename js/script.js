8d7b2ac080589601d0052ae4efa0c8d7a77716ff
gilani-khas <gilani95316@gmail.com>
2023-01-25 12:22:53 +0300

    Добавил переменные и if

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-let a = 12;
-let b = 36;
-console.log(a * b);
+const MY_CONST = "55";
+
+if (MY_CONST < 10) {
+  console.log("Константа меньше 10");
+} else {
+  console.log("Константа больше 10");
+}
+
+let a = 2;
+let b = 9;
+
+if (a < b) {
+  console.log("code");
+} else if (a > b) {
+  console.log("nocode");
+} else {
+  console.log("error");
+}
+
+let dataLogin = "Ali";
+let dataPas = "1234";
+
+let login = prompt("login");
+let password = prompt("password");
+
+if (dataLogin === login && dataPas === password) {
+  console.log("welcome");
+} else {
+  console.log("Error");
+}
